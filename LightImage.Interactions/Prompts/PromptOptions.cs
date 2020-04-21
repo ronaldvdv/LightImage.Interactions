f49f214a40a0ee24ccc23c744c9f2059ca437c99
Ronald van der Velden <ronald.van.der.velden@gmail.com>
2020-04-21 12:11:39 +0200

    Fix issue with PromptOptions

--- LightImage.Interactions/Prompts/PromptOptions.cs
+++ LightImage.Interactions/Prompts/PromptOptions.cs
@@ -3,7 +3,7 @@ using System;
 
 namespace LightImage.Interactions.Prompts
 {
-    public class PromptOptions : MessageOptions, IInteractionInput<PromptResult>
+    public class PromptOptions : MessageOptionsBase, IInteractionInput<PromptResult>
     {
         public Predicate<string> Predicate { get; set; }
         public string Value { get; set; }
