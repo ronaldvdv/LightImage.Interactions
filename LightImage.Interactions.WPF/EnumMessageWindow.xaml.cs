71d193aad81b7547e35a95d0a9c023d0c52deee8
Ronald van der Velden <ronald.van.der.velden@gmail.com>
2020-04-28 13:01:55 +0200

    Sample dialog implementations

---
+++ LightImage.Interactions.WPF/EnumMessageWindow.xaml.cs
@@ -0,0 +1,48 @@
+﻿using LightImage.Interactions.Enums;
+using System.Linq;
+
+namespace LightImage.Interactions
+{
+    /// <summary>
+    /// Interaction logic for EnumMessageWindow.xaml
+    /// </summary>
+    public partial class EnumMessageWindow : EnumMessageWindowBase
+    {
+        private EnumViewModel _input;
+
+        public EnumMessageWindow()
+        {
+            InitializeComponent();
+        }
+
+        protected override EnumMemberViewModel GetOutput()
+        {
+            var result = _input.Members.FirstOrDefault(m => m.IsSelected);
+            return DialogResult == true ? result : null;
+        }
+
+        protected override void SetInput(EnumViewModel input)
+        {
+            Title = input.Title;
+            TextLabel.Text = input.Message;
+            RadioItems.ItemsSource = input.Members;
+            _input = input;
+        }
+
+        private void CancelButton_Click(object sender, System.Windows.RoutedEventArgs e)
+        {
+            DialogResult = false;
+            Close();
+        }
+
+        private void OkButton_Click(object sender, System.Windows.RoutedEventArgs e)
+        {
+            DialogResult = true;
+            Close();
+        }
+    }
+
+    public abstract class EnumMessageWindowBase : InteractionWindow<EnumViewModel, EnumMemberViewModel>
+    {
+    }
+}
---
+++ LightImage.Interactions.WPF/MessageWindow.xaml.cs
@@ -0,0 +1,106 @@
+﻿using LightImage.Interactions.Messages;
+using LightImage.Interactions.Prompts;
+using MahApps.Metro.IconPacks;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace LightImage.Interactions
+{
+    /// <summary>
+    /// Interaction logic for MessageWindow.xaml
+    /// </summary>
+    public partial class MessageWindow : Window,
+        IInteractionHandler<MessageOptions, MessageResult>,
+        IInteractionHandler<PromptOptions, PromptResult>
+    {
+        private readonly IReadOnlyDictionary<MessageIcons, PackIconMaterialKind> _iconMap = new Dictionary<MessageIcons, PackIconMaterialKind>
+        {
+            { MessageIcons.Edit, PackIconMaterialKind.Pencil },
+            { MessageIcons.Information, PackIconMaterialKind.Information },
+            { MessageIcons.Question, PackIconMaterialKind.CommentQuestion },
+            { MessageIcons.Warning, PackIconMaterialKind.AlertCircleOutline },
+            { MessageIcons.Error, PackIconMaterialKind.AlertOctagon },
+        };
+
+        private Predicate<string> _accept;
+
+        public MessageWindow()
+        {
+            InitializeComponent();
+        }
+
+        public MessageButton Button { get; private set; } = default;
+
+        public async Task<MessageResult> Handle(MessageOptions request, CancellationToken cancellationToken)
+        {
+            SetInput(request, null, false, string.Empty);
+            await this.ShowDialogAsync();
+            return new MessageResult(Button);
+        }
+
+        public async Task<PromptResult> Handle(PromptOptions request, CancellationToken cancellationToken)
+        {
+            SetInput(request, request.Predicate, true, request.Value);
+            var ok = true == await this.ShowDialogAsync();
+            return new PromptResult(ok ? TheTextBox.Text : null, Button);
+        }
+
+        private void HandleButton(MessageButton button)
+        {
+            Button = button;
+            DialogResult = button == MessageButton.Yes || button == MessageButton.Ok;
+            Close();
+        }
+
+        private void InitializeButton(Button control, MessageButton button, MessageButton show)
+        {
+            control.Visibility = (show & button) == button ? Visibility.Visible : Visibility.Collapsed;
+            control.Click += (sender, args) => HandleButton(button);
+        }
+
+        private void SetInput(MessageOptionsBase options, Predicate<string> predicate, bool prompt, string defaultValue)
+        {
+            Title = options.Title;
+            TheLabel.Content = options.Message;
+            TheTextBox.Text = defaultValue;
+            TheIcon.Kind = _iconMap[options.Icon];
+            _accept = predicate ?? new Predicate<string>(_ => true);
+
+            TheTextBox.Visibility = prompt ? Visibility.Visible : Visibility.Collapsed;
+
+            InitializeButton(Yes, MessageButton.Yes, options.Buttons);
+            InitializeButton(No, MessageButton.No, options.Buttons);
+            InitializeButton(OK, MessageButton.Ok, options.Buttons);
+            InitializeButton(Cancel, MessageButton.Cancel, options.Buttons);
+
+            var buttons = new[] { Yes, No, OK, Cancel };
+            var firstButton = buttons.First(b => b.Visibility == Visibility.Visible);
+            firstButton.IsDefault = true;
+            if (!prompt)
+            {
+                firstButton.Focus();
+            }
+
+            UpdateOK();
+            TheTextBox.Focus();
+        }
+
+        private void TheTextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            if (OK != null && _accept != null)
+                UpdateOK();
+        }
+
+        private void UpdateOK()
+        {
+            var accept = _accept(TheTextBox.Text);
+            OK.IsEnabled = accept;
+            Yes.IsEnabled = accept;
+        }
+    }
+}
